da2f789b03829aacbb9c91ac830bfa3bd8e734c9
CoderSid007 <siddhesdas2020@gmail.com>
2024-09-27 20:20:24 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/bookmygo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bookmygo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/json/hotel_list.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.jpg',
+    'place': 'Open Space',
+    'destination': 'Dubai',
+    'price': 25
+  },
+  {
+    'image': 'two.jpg',
+    'place': 'Merlin meranda',
+    'destination': 'Kolkata',
+    'price': 40
+  },
+];
